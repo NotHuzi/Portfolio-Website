75e0d639c4c31ac4fa181c0cb510d1f34596af43
NotHuzi <43522912+NotHuzi@users.noreply.github.com>
2019-06-12 23:14:23 +0100

    Add files via upload

---
+++ Website/contact.php
@@ -0,0 +1,34 @@
+<section class="contact" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h2>Contact</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+                    <form>
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" id="nama" class="form-control" placeholder="Masukan Nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="email" id="email" class="form-control" placeholder="Masukan Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="telp">Telepon</label>
+                            <input type="tel" id="telp" class="form-control" placeholder="Masukan No Telepon">
+                        </div>
+                        <div class="from-group">
+                            <label for="pesan">Pesan</label>
+                            <textarea class="form-control" placeholder="Masukan Pesan" rows="10"></textarea>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Kirim Pesan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
---
+++ Website/js/index.js
@@ -0,0 +1,27 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
